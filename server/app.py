35091dfa135585228f6d9f3e01e276e6fa62b8a2
mohammedhassa15 <mohammedhassan0041@gmail.com>
2025-09-18 21:28:49 +0300

    password protection

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, make_response,  jsonify
 from flask_restful import Resource
 
 from config import app, db, api
@@ -28,16 +28,51 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if  session.get('user_id'):
+             user = User.query.filter(User.id == session['user_id']).first()
+             return user.to_dict(), 200
+        
+        return {}, 204
+
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id 
+            return user.to_dict(), 200
+
+        return {'error': '401, Unauthorized'}, 401
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
+
+
+
+
+
+
+
+
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint = 'login')
+api.add_resource(Logout, '/logout',endpoint = 'logout')
+api.add_resource(CheckSession, '/check_session', endpoint = 'check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
